a783f1d939637b59317c3fc8b17f2b608520af8e
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-04-26 16:36:57 +0545

    learnt about global namespace

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-console.log("Hello its me index.js");
+// console.log("Hello its me index.js");
 
-console.log(2 === "2");
+// console.log(2 === "2");
 
-function addTwoNumber(a, b){
-    return a + b;
+// function addTwoNumber(a, b){
+//     return a + b;
+// }
+
+// console.log(addTwoNumber(5, 5));
+
+/* GLOBAL in nodejs */
+global.companyName = "xyz company";
+
+function print(){
+    /* We can declare global variable without using global keyword */
+    programName = "javascript";
+    console.log(companyName);
 }
 
-console.log(addTwoNumber(5, 5));
+print();
+console.log(programName);
