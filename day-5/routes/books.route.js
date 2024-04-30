179bf9979f47ca82b1a7a27e31bb82041b31fe19
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-04-30 12:33:40 +0545

    learnt more about request and completed day 5

--- day-5/routes/books.route.js
+++ day-5/routes/books.route.js
@@ -3,15 +3,18 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.status(200).send("Books pagess");
-})
+  const { id, author } = req.query;
+
+  res.status(200).send("Books pagess");
+});
 
 router.get("/add", (req, res) => {
-    res.status(200).json({added: "yes true"})
-})
+  res.status(200).json({ added: "yes true" });
+});
 
-router.get("/delete", (req, res) => {
-    res.status(200).json({deleted: true})
-})
+router.get("/delete/:id", (req, res) => {
+    console.log(req.params);
+  res.status(200).json({ deleted: true });
+});
 
 export default router;
