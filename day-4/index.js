a54f9b62c182269d3f88fb5a2301fba6de2b7e4c
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-04-30 10:16:14 +0545

    learn request parameter and install nodemon

--- day-4/index.js
+++ day-4/index.js
@@ -1,8 +1,23 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end("Backend is working");
+    switch(req.url){
+        case "/books": 
+        console.log(req.method);
+        res.writeHead(200);
+        res.end("Books page");
+        break;
+        
+        case "/others": 
+        res.writeHead(200);
+        res.end("Others page");
+        break;
+
+        default: 
+        res.writeHead(200);
+        res.end("Backend is working");
+        break;
+    }
 })
 
 server.listen(8000, () => {
