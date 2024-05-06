5553474b54df773264fa3cef97ec7a76e68cad6f
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-05-06 23:33:50 +0545

    setup sequelize and connect to database

--- project-one/src/index.js
+++ project-one/src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import connection from "./models/index.js";
 
 const app = express();
 
@@ -6,6 +7,13 @@ app.use("/", (req, res) => {
     res.send("Backend is working");
 })
 
-app.listen(8000, () => {
+app.listen(8000, async() => {
     console.log("Server has be started 🚀");
+
+    try{
+        await connection.authenticate();
+        console.log("Successfully connected to DB");
+    }catch(err){
+        console.error("Uanble to connect to the database ", err);
+    }
 })
