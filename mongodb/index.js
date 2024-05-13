d4d09e3d138f975b88cde1f1d203620a636c710b
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-05-13 17:17:44 +0545

    setup mongodb, add add-book route, add schema and model for database

--- mongodb/index.js
+++ mongodb/index.js
@@ -1,7 +1,31 @@
 import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+import blogModel from "./models/schema.js";
 
 const app = express();
 
-app.listen(8000, () => {
-    console.log("Server has started!🚀");
+app.get("/add-book", async(req, res) => {
+    try{
+        const response = await blogModel.create({
+            title: "Title Two",
+            description: "Description Two",
+        })
+        res.json(response)
+    }catch(err){
+        res.json({
+            success: false,
+            message: err.message
+        })
+    }
 })
+
+app.listen(8000, async () => {
+  console.log("Server has started!🚀");
+  try {
+    await mongoose.connect(process.env.MONGODB_CONNECTION_URL);
+    console.log("Connected t database💻");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
