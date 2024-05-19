7ab2a4f166f6a2b3edecd3ddd4b895732f11692a
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-05-19 09:53:00 +0545

    create view pages and render them using server

--- template_engines/index.js
+++ template_engines/index.js
@@ -1,9 +1,19 @@
 import express from "express"
+import {fileURLToPath} from "url";
+import { dirname } from "path";
 
 const app = express();
+app.use(express.static("public"))
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.get("/", (req, res) => {
-    res.send("Hello")
+    res.sendFile(__dirname + "/view/homepage.html");
+})
+
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/view/about.html")
 })
 
 app.listen(8000, () => {
