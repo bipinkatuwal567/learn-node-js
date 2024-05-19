dbc6c8a49cff60dae23020e1673b0d283bae2bfc
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-05-19 10:44:19 +0545

    render view page using ejs library

--- template_engines/index.js
+++ template_engines/index.js
@@ -4,16 +4,32 @@ import { dirname } from "path";
 
 const app = express();
 app.use(express.static("public"))
+app.set('view engine', 'ejs')
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/view/homepage.html");
+   res.render("homepage", {
+    title: "Home"
+   }) 
 })
 
 app.get("/about", (req, res) => {
-    res.sendFile(__dirname + "/view/about.html")
+    res.render("about", {
+        title: "About us"
+    })
+})
+
+app.get("/services", (req, res) => {
+    res.render("services", {
+        title: "Services",
+        serviceList: ["Web Design", "SEO", "App Development"]
+    })
+})
+
+app.use((req, res) => {
+    res.send("<p>Page not found</p>")
 })
 
 app.listen(8000, () => {
