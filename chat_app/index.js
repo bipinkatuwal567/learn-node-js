2930ab975885c0f3addb7aa9bd20f25c33873a1d
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-05-21 15:05:20 +0545

    complete simple chat app with socket.io

--- chat_app/index.js
+++ chat_app/index.js
@@ -1,13 +1,32 @@
-import express from 'express';
-import { createServer } from 'node:http';
+import express from "express";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
 });
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+    console.log("Message : ", msg);
+  });
+});
+
+server.listen(8000, () => {
+  console.log("server running at http://localhost:8000");
 });
